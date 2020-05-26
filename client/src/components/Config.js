9f119a6216a2399e5ffb482a42b34253212cf237
jimmy <ronaldoyoung@gmail.com>
2020-05-27 00:12:21 +0900

    랜딩 페이지 메인이미지

--- client/src/components/Config.js
+++ client/src/components/Config.js
@@ -2,5 +2,5 @@
 export const USER_SERVER = '/api/users';
 
 export const API_URL = 'https://api.themoviedb.org/3/';
-
+export const API_KEY = 'da1c7e65946b85d988cc1aa882ed8df0';
 export const IMAGE_BASE_URL = 'http://image.tmdb.org/t/p/';
