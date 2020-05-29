f6d2a59739398f9f7264989d50be7177f5815f31
jimmy <ronaldoyoung@gmail.com>
2020-05-30 00:42:13 +0900

    grid card 생성

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -2,6 +2,8 @@ import React from 'react'
 import { useEffect, useState } from 'react';
 import { API_URL, API_KEY, IMAGE_BASE_URL } from '../../Config';
 import MainImage from './Sections/MainImage';
+import GridCards from '../commons/GridCards';
+import { Row } from 'antd';
 
 function LandingPage() {
 
@@ -16,7 +18,7 @@ function LandingPage() {
             .then(response => {
 
                 console.log('@@' , response.results);
-                setMovies([response.results])
+                setMovies(response.results)
                 setMainMovieImage(response.results[0]);
             })
 
@@ -34,10 +36,21 @@ function LandingPage() {
             }
 
             <div style={{ width: '85%', margin: '1rem auto'}}>
-                <h2>Movies by latest</h2>
+                <h2>Movies by latest!</h2>
                 <hr />
                 {/* Movie Grid Cards */}
-
+                <Row>
+                  {Movies && Movies.map((movie, index) => (
+                      <React.Fragment key={index}>
+                          <GridCards
+                            image={movie.poster_path ?
+                              `${IMAGE_BASE_URL}w500${movie.poster_path}` : null }
+                            movieId={movie.id}
+                            movieName={movie.original_title}
+                          />
+                      </React.Fragment>
+                  ))}
+                </Row>
             </div>
 
             <div style={{ display: 'flex', justifyContent: 'center'}}>
---
+++ client/src/components/views/commons/GridCards.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Col } from 'antd';
+
+function GridCards (props){
+  console.log('asdfadf' , props);
+  return (
+    <div>
+      <Col lg={6} md={8} xs={24}>
+        <div style={{ position: 'relative' }}>
+          <a href={`/movie/${props.movieId}`}>
+            <img src={props.image} alt={props.movieName} style={{ width: '100%', height: '3' }}/>
+          </a>
+        </div>
+      </Col>
+    </div>
+  );
+}
+
+export default GridCards;
