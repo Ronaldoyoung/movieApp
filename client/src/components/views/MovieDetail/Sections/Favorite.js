86474ef63d58426de280de9fd8b17cd9646680b9
jimmy <ronaldoyoung@gmail.com>
2020-05-30 23:28:12 +0900

    DB에서 Favorite 정보 가져오기

--- client/src/components/views/MovieDetail/Sections/Favorite.js
+++ client/src/components/views/MovieDetail/Sections/Favorite.js
@@ -18,14 +18,14 @@ const Favorite = (props) => {
 
     Axios.post('/api/favorite/favoriteNumber', variables)
       .then(response => {
+        console.log(response.data);
         if(response.data.success){
-
         }else{
           alert('숫자 정보를 가져오는데 실패했습니다.')
         }
       })
 
-  }, [input]);
+  }, []);
 
 
   return (
