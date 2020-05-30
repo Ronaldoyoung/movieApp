e57e8f0a3410208037f501ab02d72b05b9b452c7
jimmy <ronaldoyoung@gmail.com>
2020-05-30 23:53:57 +0900

    favorite 버튼 기능 구현

--- client/src/components/views/MovieDetail/Sections/Favorite.js
+++ client/src/components/views/MovieDetail/Sections/Favorite.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import Axios from 'axios';
 
 const Favorite = (props) => {
@@ -8,6 +8,9 @@ const Favorite = (props) => {
   const movieTitle = props.movieTitle;
   const moviePost = props.movieInfo.backdrop_path;
   const movieRunTime = props.movieInfo.runtime;
+
+  const [FavoriteNumber, setFavoriteNumber] = useState(0);
+  const [Favorited, setFavorited] = useState(false);
 
   useEffect(() => {
 
@@ -20,8 +23,19 @@ const Favorite = (props) => {
       .then(response => {
         console.log(response.data);
         if(response.data.success){
+          setFavoriteNumber(response.data.favoriteNumber);
         }else{
           alert('숫자 정보를 가져오는데 실패했습니다.')
+        }
+      })
+
+    Axios.post('/api/favorite/favorited', variables)
+      .then(response => {
+
+        if(response.data.success){
+          setFavorited(response.data.favorited);
+        }else{
+          alert('정보를 가져오는데 실패 했습니다.')
         }
       })
 
@@ -29,7 +43,7 @@ const Favorite = (props) => {
 
 
   return (
-      <button>Favorite</button>
+  <button>{Favorited ? "Not Favorite": "Add to Favorite "} {FavoriteNumber}</button>
   );
 }
 
